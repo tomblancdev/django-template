db4e397eed6bc5308d6e0012f48c2e1fd50dfb5c
Tom <dev@tomblanc.fr>
2025-02-17 18:13:31 +0000

    Display a welcome message in the terminal upon devcontainer startup and remove pre-commit autoupdate Fixes #11

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
 
-# Update pre-commit if there is a git repository
-if [ -d .git ]; then
-    pre-commit autoupdate
-fi
+# Display an Hello message to the user in the terminal
+echo """
+
+ _   _      _ _
+| | | |    | | |
+| |_| | ___| | | ___
+|  _  |/ _ \ | |/ _ \
+| | | |  __/ | | (_) |
+\_| |_/\___|_|_|\___/
+
+Welcome to the devcontainer!
+"""
 
 exit 0
